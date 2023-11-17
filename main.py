273de47aa063b8dac1c07cd00bcdfa188b55798f
Sebastian Zavala <77974004+Sebastianzvla@users.noreply.github.com>
2023-11-17 14:00:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,12 @@ elif Columa == "b":
 elif Columa == "c":
   map[x-1][2] = "X"
   
+  #Alternativa
+# letter = position[0].lower()
+# abc = ["a", "b", "c"]
+# letter_index = abc.index(letter)
+# number_index = int(position[1]) - 1
+# map[number_index][letter_index] = "X"
+
 print(" ")
 print(f"{line1}\n{line2}\n{line3}")
